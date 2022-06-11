90c712bf744fac8ed0bfacd6bee32142c1c5219b
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-06-12 01:43:29 +0300

    Create About.cs

---
+++ src/dyn2ifc/About.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using dg = Autodesk.DesignScript.Geometry;
+using dr = Autodesk.DesignScript.Runtime;
+
+using GeometryGym.Ifc;
+
+namespace dyn2ifc
+{
+    [dr.IsVisibleInDynamoLibrary(false)]
+    public class About
+    {
+        /// <summary>
+        /// About package - link to GitHub repository
+        /// </summary>
+        /// <returns></returns>
+        [dr.IsVisibleInDynamoLibrary(true)]
+        public static string GetInfoAboutPackage()
+        {
+            return "Look repository of package https://github.com/GeorgGrebenyuk/dyn2ifc";
+        }
+    }
+}
