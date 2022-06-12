8178563f52c4c31c26d39847098db59bc52b7546
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-06-12 23:31:53 +0300

    Add classes IfcGrouping and IfcMaterialSet
    IfcGrouping contains IfcBuildingStorey -- for save data at storeys;
    IfcMaterialSet contain no work now node to set material (color) to element

---
+++ src/dyn2ifc/IfcGrouping.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using dg = Autodesk.DesignScript.Geometry;
+using dr = Autodesk.DesignScript.Runtime;
+
+using GeometryGym.Ifc;
+using static dyn2ifc.IfcFile.IfcDoc;
+
+namespace dyn2ifc
+{
+    [dr.IsVisibleInDynamoLibrary(true)]
+    public class IfcGrouping
+    {
+        private IfcObjectDefinition group;
+        private IfcBuildingStorey storey;
+        /// <summary>
+        /// Getting IfcGroup from IfcGrouping class
+        /// </summary>
+        /// <returns>IfcGroup</returns>
+        [dr.IsVisibleInDynamoLibrary(true)]
+        public IfcObjectDefinition get_group()
+        {
+            return this.group;
+        }
+        [dr.IsVisibleInDynamoLibrary(true)]
+        public IfcGrouping(string name)
+        {
+            group = new IfcGroup(ifc_db, name);
+        }
+        /// <summary>
+        /// Create IfcBuildingStorey by name and elevation
+        /// </summary>
+        /// <param name="name">Name of level (storey)</param>
+        /// <param name="elevation">Elevation of level (storey) in selected units in IFC</param>
+        [dr.IsVisibleInDynamoLibrary(false)]
+        public IfcGrouping(string name, double elevation)
+        {
+            group = new IfcBuildingStorey(ifc_site, name, elevation);
+        }
+        /// <summary>
+        /// Add created IfcElements to group
+        /// </summary>
+        /// <param name="group">Result of node IfcGrouping.get_group</param>
+        /// <param name="elements">Collections of IfcElement</param>
+        /// <returns></returns>
+        [dr.IsVisibleInDynamoLibrary(false)]
+        public static int AddIdcElementsToGroup(int order, IfcObjectDefinition group, List<IfcElement> elements)
+        {
+            foreach (IfcElement elem in elements)
+            {
+                group.AddNested(elem);
+            }
+            return order;
+        }
+    }
+}
---
+++ src/dyn2ifc/IfcMaterialSet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using dg = Autodesk.DesignScript.Geometry;
+using dr = Autodesk.DesignScript.Runtime;
+
+using GeometryGym.Ifc;
+using static dyn2ifc.IfcFile.IfcDoc;
+
+
+namespace dyn2ifc
+{
+    [dr.IsVisibleInDynamoLibrary(true)]
+    public class IfcMaterialSet
+    {
+        private IfcMaterial material;
+        /// <summary>
+        /// Getting an IfcMaterial from IfcMaterialSet class
+        /// </summary>
+        /// <returns></returns>
+        [dr.IsVisibleInDynamoLibrary(true)]
+        public IfcMaterial get_material()
+        {
+            return this.material;
+        }
+        /// <summary>
+        /// Create 
+        /// </summary>
+        /// <param name="color_data">Color info (double array with 3 values)</param>
+        /// <param name="material_name">Name of your material</param>
+        [dr.IsVisibleInDynamoLibrary(true)]
+        public IfcMaterialSet(double[] color_data, string material_name)
+        {
+            //problem ... how set a color???
+            IfcColourRgb ifc_color = new IfcColourRgb(ifc_db, color_data[0] / 256d, color_data[1] / 256d, color_data[2] / 256d);
+            IfcSurfaceStyleRendering ifc_style1 = new IfcSurfaceStyleRendering(ifc_color);
+            //IfcSurfaceStyleShading ifc_style = new IfcSurfaceStyleShading(ifc_color);
+            IfcSurfaceStyle ifc_style0 = new IfcSurfaceStyle(ifc_style1);
+            ifc_style0.Name = $"some_material_rgb_{color_data[0]}-{color_data[1]}-{color_data[2]}";
+
+            this.material = new IfcMaterial(ifc_db, material_name);
+            IfcPresentationStyleAssignment style_assignm = new IfcPresentationStyleAssignment(ifc_style0);
+            IfcStyledItem object_style = new IfcStyledItem(style_assignm);
+            IfcStyledRepresentation repr = new IfcStyledRepresentation(object_style);
+
+            IfcMaterialDefinitionRepresentation ifc_def = new IfcMaterialDefinitionRepresentation(repr, material);
+        }
+
+    }
+}
